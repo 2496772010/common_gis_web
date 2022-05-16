5941b32acdb602b442a7495505a638979b6476a1
gis_rc <chao.ran@southgis.com>
2022-05-16 22:20:26 +0800

    新增app

---
+++ src/pages/trafficPoint/trafficPointApp.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="trafficPointApp">
+    <base-map :params="mapParams"></base-map>
+  
+  </div>
+</template>
+
+<script>
+
+  import BaseMap from "../../components/BaseMap";
+  import {mapParams} from "../../config/mapConfig";
+
+  export default {
+    name: "trafficPointApp",
+    components: {BaseMap},
+    data() {
+      return {
+        mapParams
+      }
+    },
+    methods: {}
+  }
+</script>
+
+<style scoped>
+  #trafficPointApp {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    padding: 0;
+    margin: 0;
+    
+  }
+</style>
+
