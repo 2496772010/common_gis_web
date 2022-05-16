8b62ed349e4ab209dd4acdeb40c2cb447fb81eb5
Administrator <gis_rc@163.com>
2022-05-16 18:09:00 +0800

    添加新应用

---
+++ src/pages/trafficPoints/trafficPointsApp.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="trafficPointsApp">
+        <base-map ></base-map>
+
+    </div>
+</template>
+
+<script>
+
+    import BaseMap from "../../components/BaseMap";
+
+    export default {
+        name: "trafficPointsApp",
+        components: {BaseMap},
+        data() {
+            return {}
+        },
+        methods: {}
+    }
+</script>
+
+<style scoped>
+    #trafficPointsApp {
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        padding: 0;
+        margin: 0;
+
+    }
+</style>
+
