8f1e3b07d7f1857cf66a6b05990309f2c76a7934
Administrator <gis_rc@163.com>
2022-05-23 12:29:19 +0800

    trafficapp添加绘制

--- src/pages/trafficPoints/trafficPoints.js
+++ src/pages/trafficPoints/trafficPoints.js
@@ -1,14 +1,14 @@
 import {createApp} from 'vue'
 import {
-  ElButton,
-  ElAutocomplete,
-  ElAside,
-  ElContainer,
-  ElMain,
-  ElMenu,
-  ElMenuItem,
-  ElSubmenu, ElInput, ElInputNumber,
-  ElSelect, ElOption, ElForm, ElFormItem, ElTable, ElTableColumn, ElDialog
+    ElButton,
+    ElAutocomplete,
+    ElAside,
+    ElContainer,
+    ElMain,
+    ElMenu,
+    ElMenuItem,
+    ElSubmenu, ElInput, ElInputNumber,
+    ElSelect, ElOption, ElForm, ElFormItem, ElTable, ElTableColumn, ElDialog, ElColorPicker
 } from 'element-plus';
 import App from './trafficPointsApp.vue';
 // 如果要使用.scss样式文件，则需要引入base.scss文件
@@ -28,10 +28,11 @@ app.component(ElInput.name, ElInput)
 app.component(ElInputNumber.name, ElInputNumber)
 app.component(ElSelect.name, ElSelect)
 app.component(ElOption.name, ElOption)
-app.component(ElForm.name,ElForm)
-app.component(ElFormItem.name,ElFormItem)
-app.component(ElTable.name,ElTable)
-app.component(ElTableColumn.name,ElTableColumn)
-app.component(ElDialog.name,ElDialog)
+app.component(ElForm.name, ElForm)
+app.component(ElFormItem.name, ElFormItem)
+app.component(ElTable.name, ElTable)
+app.component(ElTableColumn.name, ElTableColumn)
+app.component(ElDialog.name, ElDialog)
+app.component(ElColorPicker.name, ElColorPicker)
 app.mount('#trafficPointsApp')
 
--- src/pages/trafficPoints/trafficPointsApp.vue
+++ src/pages/trafficPoints/trafficPointsApp.vue
@@ -8,12 +8,49 @@
                 width="30%"
                 show-close
                 draggable
+                :close-on-click-modal="false"
+
         >
             <el-form label-suffix="：">
-                <el-form-item label="点类型"></el-form-item>
+                <el-form-item label="点类型">
+                    <el-select v-model="value"
+                               @change="onChange"
+                               placeholder="请输入或选择类型"
+                               allow-create clearable filterable>
+                        <el-option
+                                v-for="(item,index) in types"
+                                :value="item"
+                                :label="item"
+                                :key="index"></el-option>
+                    </el-select>
+                </el-form-item>
+                <el-form-item label="点颜色">
+                    <el-color-picker color-format="rgb" v-model="color"></el-color-picker>
+                </el-form-item>
             </el-form>
+            <template #footer>
+      <span class="dialog-footer">
+        <el-button @click="dialogVisible = false">取消</el-button>
+        <el-button type="primary" @click="onConfirmClick"
+        >确定</el-button
+        >
+      </span>
+            </template>
         </el-dialog>
-
+        <el-table
+                style="position: fixed;right: 10px;bottom: 10px;width: 200px;max-height: 30vh;overflow-x: hidden "
+                :data="features"
+                fit
+                height="200"
+                v-show="features.length"
+        >
+            <el-table-column prop="type" label="事件类型"></el-table-column>
+            <el-table-column prop="color" label="颜色">
+                <template #default="scope">
+                    <div style="width: 40px;height: 20px" :style="{background:scope.row.color}"></div>
+                </template>
+            </el-table-column>
+        </el-table>
     </div>
 </template>
 
@@ -21,6 +58,9 @@
 
     import BaseMap from "../../components/BaseMap";
     import EditbarMinxin from "../../minxins/EditbarMinxin";
+    import locationPng from './img/loc.png'
+    import dotPng from './img/dot.png'
+    import {Style, Fill, Circle, Stroke} from 'ol/style'
 
     export default {
         name: "trafficPointsApp",
@@ -28,7 +68,16 @@
         mixins: [EditbarMinxin],
         data() {
             return {
-                dialogVisible: false
+                dialogVisible: false,
+                value: '出租车',
+                types: ['出租车'],
+                images: {
+                    locationPng,
+                    dotPng
+                },
+                color: 'rgb(255,0,0)',
+                feature: null,
+                features: []
             }
         },
         mounted() {
@@ -38,16 +87,49 @@
             onMapCreated(map) {
                 this.initMyMap(map)
                 this.m_editBar.getInteraction('DrawPoint').on('drawstart', (e) => {
-                    console.log(e)
                     this.dialogVisible = true
-                    // this.m_tooltip.setInfo(e.oldValue ? '' : 'Click map to place a point...');
+                    this.feature = e.feature
                 });
+            },
+            onConfirmClick() {
+                this.feature.setStyle(new Style({
+                    image: new Circle({
+                        radius: 5,
+                        fill: new Fill({
+                            color: this.color
+                        })
+                    }),
+                    stroke: new Stroke({
+                        width: 1,
+                        color: 'rgb(0,0,0)'
+                    })
+
+                }))
+                this.feature.setProperties({
+                    type: this.value
+                })
+                this.dialogVisible = false
+                if (!this.features.filter(value => {
+                    return value.color === this.color && this.value === value.type
+                }).length) {
+                    this.features.push({
+                        feature: this.feature,
+                        color: this.color,
+                        type: this.value
+                    })
+
+                }
+            },
+            onChange(value) {
+                if (this.types.indexOf(value) < 0) {
+                    this.types.push(value)
+                }
             }
         }
     }
 </script>
 
-<style scoped>
+<style lang="less" scoped>
     #trafficPointsApp {
         width: 100%;
         height: 100%;
@@ -55,6 +137,16 @@
         padding: 0;
         margin: 0;
 
+    }
+
+    .img-icon {
+        transform: translateX(-100%);
+        position: relative;
+
+        img {
+            border-right: 100px solid transparent;
+            filter: drop-shadow(10px 0px 0 red)
+        }
     }
 </style>
 
