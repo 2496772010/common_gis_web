a80a37fb6e4e8b1094dfe38b52869232704b19e9
ranchao <ranchao@qq.com>
2022-07-29 10:01:22 +0800

    添加cesium实例，优化createApp.js

--- build/createApp.js
+++ build/createApp.js
@@ -16,41 +16,13 @@ const Files = [
     //main.js
     {
         fileName: `${appName}.js`,
-        content: `import {createApp} from 'vue'
-import {
-  ElButton,
-  ElAutocomplete,
-  ElAside,
-  ElContainer,
-  ElMain,
-  ElMenu,
-  ElMenuItem,
-  ElSubmenu, ElInput, ElInputNumber,
-  ElSelect, ElOption, ElForm, ElFormItem, ElTable, ElTableColumn
-} from 'element-plus';
-import App from './${appNameVue}.vue';
-// 如果要使用.scss样式文件，则需要引入base.scss文件
-import 'element-plus/lib/theme-chalk/index.css';
-// import 'element-plus/packages/theme-chalk/src/base.scss'
-
-const app = createApp(App)
-app.component(ElButton.name, ElButton)
-app.component(ElAutocomplete.name, ElAutocomplete)
-app.component(ElAside.name, ElAside)
-app.component(ElContainer.name, ElContainer)
-app.component(ElMain.name, ElMain)
-app.component(ElMenu.name, ElMenu)
-app.component(ElMenuItem.name, ElMenuItem)
-app.component(ElSubmenu.name, ElSubmenu)
-app.component(ElInput.name, ElInput)
-app.component(ElInputNumber.name, ElInputNumber)
-app.component(ElSelect.name, ElSelect)
-app.component(ElOption.name, ElOption)
-app.component(ElForm.name,ElForm)
-app.component(ElFormItem.name,ElFormItem)
-app.component(ElTable.name,ElTable)
-app.component(ElTableColumn.name,ElTableColumn)
-app.mount('#${appNameVue}')
+        content: `import { createApp } from 'vue'
+        import ElementPlus from 'element-plus'
+        import 'element-plus/dist/index.css'
+        import App from './${appNameVue}.vue'
+        const app = createApp(App)
+        app.use(ElementPlus)
+        app.mount('#${appNameVue}')
 `
     },
     //index.html
---
+++ src/pages/cesiumRiver/cesiumRiverApp.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="cesiumRiverApp">
+    
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name: "cesiumRiverApp",
+    data() {
+      return {
+        
+      }
+    },
+    methods: {
+     
+  }}
+</script>
+
+<style scoped>
+  #cesiumRiverApp {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    padding: 0;
+    margin: 0;
+    
+  }
+</style>
+
